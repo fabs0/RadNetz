6263699bb25f8c76d0e8b464c48d5a8c87acd5fb
Fabian Huber <77811859+fabs0@users.noreply.github.com>
2021-04-30 12:22:56 +0200

    Update TEST.java

--- src/TEST.java
+++ src/TEST.java
@@ -1,10 +1,37 @@
-
 public class TEST {
+    public static void main(String[] args) 
+    {
+        GRAPH radnetz = new GRAPH(20);
 
-    public TEST() {
+        //Zu Testzwecken werden die ersten sieben Knoten fest angegeben.
+        //Die Rueckgabewerte bezeichnen den Index des Knotens.
+        int a = radnetz.knotenHinzufuegen(new ORTSCHAFT("Altdorf", 100, "Marktplatz"));
+        int f = radnetz.knotenHinzufuegen(new ORTSCHAFT("Fischbach", 80, "Fischspezialitaeten"));
+        int w = radnetz.knotenHinzufuegen(new ORTSCHAFT("Weiler", 20, "Naturbad"));
+        int z = radnetz.knotenHinzufuegen(new ORTSCHAFT("Ziegelstein", 200, "Heimatmuseum"));
+        int b = radnetz.knotenHinzufuegen(new ORTSCHAFT("Burg", 50, "Historische Burg"));
+        int n = radnetz.knotenHinzufuegen(new ORTSCHAFT("Neustadt", 80, "Einkaufszentrum"));
+        int r = radnetz.knotenHinzufuegen(new ORTSCHAFT("Rain", 50, "Klettergarten"));
+
+        //Zu Testzwecken werden Kanten zwischen obigen Knoten festgelegt.
+        radnetz.kanteHinzufuegen(a,f,20);
+        radnetz.kanteHinzufuegen(a,w,15);
+        radnetz.kanteHinzufuegen(f,a,10);
+        radnetz.kanteHinzufuegen(f,w,10);
+        radnetz.kanteHinzufuegen(w,f,25);
+        radnetz.kanteHinzufuegen(w,z,5);
+        radnetz.kanteHinzufuegen(z,f,15);
+        radnetz.kanteHinzufuegen(z,b,20);
+        radnetz.kanteHinzufuegen(b,z,5);
+        radnetz.kanteHinzufuegen(b,f,15);
+        radnetz.kanteHinzufuegen(z,n,15);
+        radnetz.kanteHinzufuegen(n,r,20);
+        radnetz.kanteHinzufuegen(r,b,25);
+
+        radnetz.tiefensucheStarten(z);
+
+
 
 
     }
-
-
 }
